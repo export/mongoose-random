97aabb03e031b20d51ba86b90457496c7eb4f4ed
Selçuk Sevinç <selcuk@efabrika.com>
2015-04-07 10:49:38 +0300

    v0.1.2
    * random path as array

--- index.js
+++ index.js
@@ -1,56 +1,35 @@
-/**
- * @method random
- * @param {mongoose.Schema} schema
- * @param {Object}          options
- * @param {Function}        [options.fn=Math.random]
- * @param {String}          [options.path='random']
- */
 function random(schema, options) {
 	options = options || {};
 	var randFn = options.fn || Math.random;
-	var randCoords = function () { return [randFn(), randFn()] }
+	var randCoords = function () { return [randFn(), randFn()] };
 	var path = options.path || 'random';
+
 	var field = {};
-	field[path] = {
-		type: { type: String, default: 'Point' },
-		coordinates: { type: [Number], default: randCoords }
-	};
+	field[path] = [{type: Number}];
+
 	var index = {};
 	index[path] = '2dsphere';
 
 	schema.add(field);
 	schema.index(index);
 
-	/**
-	 * @method findRandom
-	 * @param {Object}    conditions
-	 * @param {Object}    [fields]
-	 * @param {Object}    [options]
-	 * @param {Function}  [callback]
-	 */
-	schema.statics.findRandom = function (conditions, fields, options, callback) {
-		var self = this;
-
-		if (!conditions || typeof conditions === 'function') {
+	schema.statics.qRand = function (conditions) {
+		if ( ! conditions || typeof conditions === 'function')
 			conditions = {};
-		}
 
 		conditions[path] = conditions[path] || {
 			$near: {
-				$geometry: { type: 'Point', coordinates: randCoords() }
+				$geometry: {
+					type: 'Point',
+					coordinates: randCoords()
+				}
 			}
 		};
 
-		var query = self.find.call(self, conditions, fields, options, callback);
-		query.__random = { path: path, query: conditions[path] };
-		return query;
+		return conditions;
 	};
 
-	/**
-	 * @method syncRandom
-	 * @param  callback
-	 */
-	schema.statics.syncRandom = function (callback) {
+	schema.statics.syncRand = function (callback) {
 		var self = this;
 		var stream = self.find({}).stream({ transform: transform });
 		var result = {
@@ -64,16 +43,14 @@ function random(schema, options) {
 			result.attempted += 1;
 			left += 1;
 			doc.save(function (err) {
-				if (err) {
+				if (err)
 					console.error(err.stack);
-				} else {
+				else
 					result.updated += 1;
-				}
 
 				left -= 1;
-				if (streamEnd && !left) {
+				if (streamEnd && !left)
 					return callback(null, result);
-				}
 			});
 		}).on('error', function (err) {
 			console.error(err.stack);
@@ -84,30 +61,10 @@ function random(schema, options) {
 	};
 
 	function transform(doc) {
-		var update = {
-			type: 'Point',
-			coordinates: randCoords()
-		};
+		var update = randCoords();
 		doc.set(path, update);
 		return doc;
 	}
 }
-module.exports = random;
 
-/**
- * Merges keys from `objects` into `base`, without overwriting.
- *
- * @method  merge
- * @param   {Object}    base
- * @param   {[Object]}  [objects]*
- * @returns {Object}
- */
-function merge(base) {
-	var objects = [].slice.call(arguments, 1);
-	objects.forEach(function (obj) {
-		Object.keys(obj).forEach(function (key) {
-			base[key] = base[key] === undefined ? obj[key] : base[key];
-		});
-	});
-	return base;
-}
+module.exports = random;
